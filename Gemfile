f7b0b4b15d3bfcec995eb159b14a3b8f42e47113
kazuhikomiyamoto <k.miyamoto.0128@gmail.com>
2015-10-15 08:01:43 +0000

    uploard picture

--- Gemfile
+++ Gemfile
@@ -20,6 +20,10 @@ gem 'faker'
 gem 'will_paginate'
 gem 'turbolinks'
 
+gem 'carrierwave',             '0.10.0'
+gem 'mini_magick',             '3.8.0'
+gem 'fog',                     '1.26.0'
+
 gem 'bootstrap-will_paginate', '0.0.10'
 
 # Use jquery as the JavaScript library
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,9 +1,22 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   
   has_many :favorites
   has_many :favorite_microposts, through: :favorites, source: :user
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+  mount_uploader :picture, PictureUploader
+  
+   private
+
+    # アップロード画像のサイズを検証する
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
+  
 end
